e76ebde5e6c8700a6a553d04038e73dfa50072e1
Evgeniy Tarasov <tarasovem@gmail.com>
2022-02-27 13:17:10 +0300

    Фикс замечаний линтера

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,7 @@ import runEngine from '../index.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getEvenResult = (num) => num % 2 === 0 ? 'yes': 'no';
+const getEvenResult = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
 const getQuestionAndAnswer = () => {
   const num = utils.getRandomInt(100);
